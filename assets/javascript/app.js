27f9c57c3fd2d1e9150d4721d2effb6fb8aaa814
Nano Bacano <ricardo.bentin@gmail.com>
2018-07-15 22:12:41 -0400

    successfully reading and writing to and from firebase. Now time to figure out the moment.js stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,38 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var now = moment();
 console.log(now);
+
+var trainName = "";
+var trainDestination = "";
+var firstTrainTime = "";
+var trainFrequency = "";
+
+
+$("#add-train-btn").on("click", function () {
+    event.preventDefault();
+    trainName = $("#train-name-input").val().trim();
+    console.log(trainName);
+    trainDestination = $("#destination-input").val().trim();
+    firstTrainTime = $("#time-input").val().trim();
+    console.log(firstTrainTime);
+    trainFrequency = $("#frequency-input").val().trim();
+
+    database.ref().push({
+        Train: trainName,
+        Destination: trainDestination,
+        First_Departure: firstTrainTime,
+        Frequency: trainFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    console.log(database);
+        
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function (snapshot) {
+    var sv = snapshot.val();
+    $("tbody").append(`<tr> <td>${sv.Train}</td><td>${sv.Destination}</td><td>${sv.Frequency}</td><td>"Next Arrival"</td><td>"Minutes Away"</td></tr>`);
+})
